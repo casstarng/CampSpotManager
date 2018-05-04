68a6ff4a5d24ced38a368aad816a1d594720916a
raghavgr94 <raghavgr94@gmail.com>
2018-05-04 07:55:45 -0700

    :tada: CampSpot and Main. Test main

--- src/CampSpot.java
+++ src/CampSpot.java
@@ -1,5 +1,60 @@
 /**
  * Created by Cassidy Tarng on 5/4/2018.
  */
+import java.util.UUID;
 public class CampSpot {
+    int campSpotID;
+    int parkingSpace;
+    int recommendedPeople;
+    int tentSpace;
+    double price;
+    boolean handicap;
+
+    public CampSpot() {
+        campSpotID = UUID.randomUUID();
+    }
+
+    public int getCampSpotID() {
+        return campSpotID;
+    }
+
+    public int getRecommendedPeople() {
+        return recommendedPeople;
+    }
+
+    public boolean isHandicap() {
+        return handicap;
+    }
+
+    public int getTentSpace() {
+        return tentSpace;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getParkingSpace() {
+        return parkingSpace;
+    }
+
+    public void setHandicap(boolean handicap) {
+        this.handicap = handicap;
+    }
+
+    public void setParkingSpace(int parkingSpace) {
+        this.parkingSpace = parkingSpace;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setRecommendedPeople(int recommendedPeople) {
+        this.recommendedPeople = recommendedPeople;
+    }
+
+    public void setTentSpace(int tentSpace) {
+        this.tentSpace = tentSpace;
+    }
 }
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+
+    public static void main(String args[]) {
+        CampSpot test= new CampSpot();
+        System.out.println(test.getCampSpotID());
+    }
+}
