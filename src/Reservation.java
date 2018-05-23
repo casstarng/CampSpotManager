203cc8c66f22595a59dc46104d7f9d88e7140af9
raghavgr94 <raghavgr94@gmail.com>
2018-05-23 08:16:53 -0700

    :rocket: reservation.java uploaded

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,5 +1,12 @@
+import java.util.Date;
+
 /**
  * Created by Cassidy Tarng on 5/4/2018.
  */
 public class Reservation {
+    int reserveID;
+    int pricePerDay;
+    Date reserveTime;
+    Date startTime;
+    Date endTime;
 }
