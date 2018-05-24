1bac6045ee13fad23f523369f80b09796d231526
Jing Li <lijingsophia@gmail.com>
2018-05-24 15:57:30 -0700

    login&register&modify&home screens

---
+++ src/FileOpe.java
@@ -0,0 +1,64 @@
+/*created by Jing Li*/
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.util.Properties;
+import javax.swing.JOptionPane;
+
+public class FileOpe {
+    private static String fileName="data/userInformation.txt";
+    private static Properties pps;
+    static {
+        pps=new Properties();
+        FileReader reader=null;
+        try {
+            reader=new FileReader(fileName);
+            pps.load(reader);
+        }
+        catch (Exception ex) {
+            JOptionPane.showMessageDialog(null,"File error",
+            	    "Inane error",
+            	    JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
+        finally {
+            try {
+                reader.close();
+            }
+            catch (Exception ex) {}
+        }
+    }
+    private static void listInfo() {
+        PrintStream ps=null;
+        try {
+            ps=new PrintStream(fileName);
+            pps.list(ps);
+        }
+        catch (Exception ex) {
+            JOptionPane.showMessageDialog(null,"File error",
+            	    "Inane error",
+            	    JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
+        finally {
+            try {
+                ps.close();
+            }
+            catch (Exception ex) {}
+        }
+    }
+    public static void getInfoByAccount(String account) {
+        String cusInfo=pps.getProperty(account);
+        if(cusInfo!=null) {
+            String[] infos=cusInfo.split("#");
+            Conf.account=account;
+            Conf.password=infos[0];
+            Conf.name=infos[1];
+            Conf.phone=infos[2];
+        }
+    }
+    public static void updateCustomer(String account,String password,
+                                      String name,String phone) {
+        pps.setProperty(account,password+"#"+name+"#"+phone);
+        listInfo();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
 
     public static void main(String args[]){
-        CampSpotManager campSpotManager = new CampSpotManager();
+        //CampSpotManager campSpotManager = new CampSpotManager();
+        new LoginFrame();
     }
 }
