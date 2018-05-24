d70c59a664f8541288031e2b7aa829409b28b0ee
Jing Li <lijingsophia@gmail.com>
2018-05-24 15:50:00 -0700

    revert

--- CampSpotManager/src/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-
-    public static void main(String args[]){
-        //CampSpotManager campSpotManager = new CampSpotManager();
-        new LoginFrame();
-    }
-}
--- CampSpotManager/src/ReservationManager.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Cassidy Tarng on 5/4/2018.
- */
-public class ReservationManager {
-}
