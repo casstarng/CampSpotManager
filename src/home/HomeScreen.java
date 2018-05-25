b76505aa547d4d192de46d013f76a696dc6ae027
AletOne <1226628804@qq.com>
2018-05-25 15:09:35 -0700

    home screen

--- src/home/HomeScreen.java
+++ src/home/HomeScreen.java
@@ -1,10 +1,24 @@
 package home;
 
+import UTIL.GUIUtil;
+
 import javax.swing.*;
-public class HomeScreen extends JFrame {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HomeScreen extends JFrame implements ActionListener{
+    
+
 
     public HomeScreen(){
         this.setSize(850, 850);
+        GUIUtil.toCenter(this);
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
